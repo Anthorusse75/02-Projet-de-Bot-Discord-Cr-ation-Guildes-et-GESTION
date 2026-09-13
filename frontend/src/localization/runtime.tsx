16b2e79db1a17ae85a181bda73f0a5df8b3c44a4
Anthorusse75 <97812101+Anthorusse75@users.noreply.github.com>
2026-09-13 13:28:05 +0200

    fix(ui-phase4): apply concise localized role title

--- frontend/src/localization/runtime.tsx
+++ frontend/src/localization/runtime.tsx
@@ -6,6 +6,7 @@ import { bootstrapPacks, en, type BootstrapLocaleCode, type LocaleCode, type Mes
 import { phase2Packs } from './phase2Catalog'
 import { phase3Packs } from './phase3Catalog'
 import { phase4Packs } from './phase4Catalog'
+import { phase4Overrides } from './phase4Overrides'
 
 export const CATALOG_VERSION = 'did-ui-v2'
 const bootstrap = Object.keys(bootstrapPacks) as BootstrapLocaleCode[]
@@ -34,11 +35,11 @@ export function validatePack(payload: unknown): MessagePack { if (!payload || ty
 function bundledPack(locale: string): Record<string, string> | undefined {
   if (!bootstrap.includes(locale as BootstrapLocaleCode)) return undefined
   const code = locale as BootstrapLocaleCode
-  return { ...bootstrapPacks[code], ...phase2Packs[code], ...phase3Packs[code], ...phase4Packs[code] }
+  return { ...bootstrapPacks[code], ...phase2Packs[code], ...phase3Packs[code], ...phase4Packs[code], ...phase4Overrides[code] }
 }
 
 const bundledResources = Object.fromEntries(
-  bootstrap.map((code) => [code, { translation: { ...bootstrapPacks[code], ...phase2Packs[code], ...phase3Packs[code], ...phase4Packs[code] } }]),
+  bootstrap.map((code) => [code, { translation: { ...bootstrapPacks[code], ...phase2Packs[code], ...phase3Packs[code], ...phase4Packs[code], ...phase4Overrides[code] } }]),
 )
 
 void i18n.init({
